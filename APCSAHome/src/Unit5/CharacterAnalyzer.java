4c028d92b68accb9ecdf1f197588250015074d51
judyqin <judyqin@judys-air.sduhsd.lan>
2018-02-13 14:14:43 -0800

    unit5

---
+++ APCSAHome/src/Unit5/CharacterAnalyzer.java
@@ -0,0 +1,92 @@
+package Unit5;
+
+import static java.lang.System.*;
+import java.util.Scanner;
+
+public class CharacterAnalyzer
+{
+	private char theChar;
+
+	public CharacterAnalyzer()
+	{
+		theChar = '0';
+
+	}
+
+	public CharacterAnalyzer(char c)
+	{
+
+		setChar(c);
+	}
+
+	public void setChar(char c)
+	{
+		theChar = c; 
+
+	}
+
+	public char getChar()
+	{
+		return theChar;
+	}
+
+	public boolean isUpper()
+	{
+		
+		if(65 <= theChar) 
+			if (theChar <= 90)
+				return true; 
+
+
+	return false;
+	
+	}
+
+	public boolean isLower( )
+	{
+
+		if(97 <= theChar) 
+			if (theChar <= 122)
+				return true; 
+
+		return false;
+	}
+	
+	public boolean isNumber( )
+	{
+		if(48 <= theChar) 
+			if (theChar <= 57)
+				return true; 
+
+		return false;
+	}	
+
+	
+	public int getASCII( )
+	{
+		return (int)theChar; 
+	}
+
+	
+	public String toString()
+	{
+ 		
+		if (isUpper())
+		{
+			return ""+getChar() + " is an uppercase character. ASCII == " + getASCII() + "\n";	  
+		}
+		if (isLower())
+		{
+			return ""+getChar() + " is a lowercase character. ASCII == " + getASCII() + "\n";	  
+		}
+		if(isNumber())
+		{
+			return ""+getChar() + " is a number. ASCII == " + getASCII() + "\n";	  
+		}
+		else
+		{
+			return null;
+		}
+		
+	}
+}
--- APCSAHome/src/Unit5/Lab05d.java
+++ APCSAHome/src/Unit5/Lab05d.java
@@ -1,5 +1,29 @@
 package Unit5;
 
-public class Lab05d {
+import static java.lang.System.*;
+import java.util.Scanner;
 
+public class Lab05d
+{
+	public static void main ( String[] args )
+	{
+		Scanner keyboard = new Scanner(System.in);
+		
+		out.print("Enter a letter :: ");
+		char letter = keyboard.next().charAt(0);
+		CharacterAnalyzer test = new CharacterAnalyzer(letter);
+		out.println(test);
+		
+		out.print("Enter a letter :: ");
+		char letter1 = keyboard.next().charAt(0);
+		CharacterAnalyzer test1 = new CharacterAnalyzer(letter1);
+		out.println(test1);
+		
+		out.print("Enter a letter :: ");
+		char letter2 = keyboard.next().charAt(0);
+		CharacterAnalyzer test2 = new CharacterAnalyzer(letter2);
+		out.println(test2);
+		
+
+	}
 }
